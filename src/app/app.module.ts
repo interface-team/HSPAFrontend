fb5ff8250e4152d2778e0af5b5a2d11687aa7a3e
riva.shy <interfaceteam@outlook.com>
2022-08-17 23:16:41 +0530

    update day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -13,11 +14,11 @@ import { PropertyDetailComponent } from './property/property-detail/property-det
 import { RentPropertyComponent } from './property/rent-property/rent-property.component';
 
 const appRoutes: Routes = [
-  { path: '', component: PropertyListComponent},
+  { path: '', component: PropertyListComponent },
   { path: 'add-property', component: AddPropertyComponent },
   { path: 'rent-property', component: PropertyListComponent },
   { path: 'property-detail/:id', component: PropertyDetailComponent },
-  { path: '**', component: PropertyDetailComponent }
+  { path: '**', component: PropertyDetailComponent },
 ];
 
 @NgModule({
@@ -30,7 +31,12 @@ const appRoutes: Routes = [
     PropertyDetailComponent,
     RentPropertyComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+  ],
   providers: [HousingService],
   bootstrap: [AppComponent],
 })
